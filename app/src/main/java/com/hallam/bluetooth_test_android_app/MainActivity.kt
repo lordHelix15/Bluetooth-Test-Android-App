7f22c3f06d999c40568e6735e6393f73f586b967
lordHelix15 <118746832+TylerHallam@users.noreply.github.com>
2025-02-03 14:21:15 -0500

    Added OnClick events for both buttons to send to each corresponding to what each button

--- app/src/main/java/com/hallam/bluetooth_test_android_app/MainActivity.kt
+++ app/src/main/java/com/hallam/bluetooth_test_android_app/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.hallam.bluetooth_test_android_app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.service.autofill.OnClickAction
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.hallam.bluetooth_test_android_app.databinding.ActivityMainBinding
+import com.hallam.bluetooth_test_android_app.databinding.FragmentSendConfirmationBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var instructions:Button
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        binding.instructions.setOnClickListener {
+            fragmentLoader(PromptList())
         }
+        binding.sendPrompt.setOnClickListener{
+
+            fragmentLoader(SendConfirmation())
+        }
+
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+    fun fragmentLoader(fragLoad: Fragment){
+        val fm = supportFragmentManager
+        val ft = fm.beginTransaction()
+        ft.replace(R.id.FragLayout, fragLoad)
+        ft.commit()
     }
 }
